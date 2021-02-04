f6b0b08ff76cdc35fb22e05c4a710b8884ecb9e7
andress20 <andress20@gmail.com>
2021-02-04 17:28:58 -0500

    add catch error from useMutation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,4 @@
+import styled from 'styled-components'
 import { 
   Button, 
   Form, 
@@ -19,10 +20,11 @@ const GET_USERS = gql`
   }
 `
 const CREATE_USER = gql`
-  mutation CreateUser( $name: String!, $email: String! ) {
+  mutation CreateUser( $name: String!, $email: String!, $password: String! ) {
     createUser(input: {
       name: $name
       email: $email
+      password: $password
     }) {
       _id
       name
@@ -31,18 +33,25 @@ const CREATE_USER = gql`
   }
 `
 
+const Container = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
 function Home () {
 
   const [name, setName] = useState('')
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
   const [passwordConfirm, setPasswordConfirm] = useState('')
-  const [ createUser, user ] = useMutation(CREATE_USER) 
+  const [errors, setErrors] = useState({})
+  const [ createUser, { loading: mutationLoading, error: mutationError }, ] = useMutation(CREATE_USER) 
 
-  const { data, loading, error } = useQuery(GET_USERS) 
+  const { data, loading: queryLoading, error: queryError } = useQuery(GET_USERS) 
 
-  if(loading) return <p>loading...</p>
-  if(error) return <p>somethign went wrong</p>
+  if(queryLoading) return <p>loading query...</p>
+  if(queryError) return <p>somethign went wrong</p>
 
   const handleChange = (e) => {
     const { id, value } = e.target
@@ -63,28 +72,37 @@ function Home () {
     }
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
-    createUser({
-      variables: { name, email }
-    })
+    try {
+      await createUser({ variables: { name, email, password }})
+    }catch(error) {
+      console.log('ups')
+    }
   }
 
   return (
     <>
-      <Form>
-        <Form.Group as={Row} controlId="name">
-          <Col xs="auto">
-            <Form.Label srOnly>
-              Nombre
-            </Form.Label>
-            <Form.Control
-              className="mb-2"
-              value={name}
-              placeholder="Nombre"
-              onChange={handleChange}
-            />
-          </Col>
+      <Container>
+        <span>{errors.account}</span>
+        <Form>
+          <Form.Group as={Row} controlId="name">
+            <Col xs="auto">
+              <Form.Label srOnly>
+                Nombre
+              </Form.Label>
+              <InputGroup className="mb-2">
+                <InputGroup.Prepend>
+                  <InputGroup.Text>@</InputGroup.Text>
+                </InputGroup.Prepend>
+              <Form.Control
+                className="mb-2"
+                value={name}
+                placeholder="Nombre"
+                onChange={handleChange}
+              />
+              </InputGroup>
+            </Col>
           </Form.Group>
           <Form.Group as={Row} controlId="email">
             <Col xs="auto">
@@ -151,7 +169,10 @@ function Home () {
               </Button>
             </Col>
           </Form.Group>
+        {mutationError && <p>Error :( try again</p>}
+        {mutationLoading && <p>Loading mutation...</p>}
       </Form>
+    </Container>
     </>
   )
 }
